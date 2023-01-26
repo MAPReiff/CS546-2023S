96bd69820238b3207841a397094c397886396776
Mitchell <me@mreiff.dev>
2023-01-26 13:41:02 -0500

    Lab1

---
+++ Assignments/Lab1/lab1.mjs
@@ -0,0 +1,102 @@
+// I pledge my honor that I have abided by the Stevens Honor System.
+
+export const questionOne = (arr) => {
+  // Implement question 1 here
+
+  let totalCube = 0; 
+
+  for (let i = 0; i < arr.length; i++) {
+    totalCube += arr[i] * arr[i] * arr[i];
+  }
+
+  // now we need to check if it is prime
+  let prime = true; // assume prime is true unless we determine it is not
+
+  if (totalCube == 0 || totalCube == 1) { // 0 and 1 are not prime
+    prime = false;
+  } else {
+    for (let i = 2; i < totalCube; i++) { // starting from 2, since 0 and 1 are taken care of, check all numbers till we hit the value
+      if (totalCube %i == 0) { // checking if we get nice division => is not a prime number as it is divisable
+        prime = false;
+        break; // end the loop
+      }
+
+    }
+  }
+
+  // now we make our object
+  let final = {
+    // totalCube: prime   // does not work :(
+  };
+
+  final[totalCube] = prime; // set the object key to the prime flag
+
+  return final; //return result
+};
+
+export const questionTwo = (numArray) => {
+  // Implement question 2 here
+
+  let isGood = true; // assume it is good to start unless otherwise determined
+  let startBad = -1; // var to store when the sequence starts to be bad
+  let endBad = -1; // var to store when the sequence becomes good again
+  let end = [] // empty array for end result
+
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] >= numArray[i+1]) { // if we encounter a situation where we are out of order (or 2 in a row)
+      isGood = false;
+      startBad = i;
+      endBad = i+1; // after we have a bad, the next should be good
+      break;
+    }
+  }
+
+  if (isGood) {
+    end = [isGood];
+  } else {
+    end = [isGood, startBad, endBad];
+  }
+
+  return end; //return result
+};
+
+export const questionThree = (obj1, obj2) => {
+  // Implement question 3 here
+  let keys1 = Object.keys(obj1); // keys in obj1
+  let keys2 = Object.keys(obj2); // keys in obj2
+  let allKeys = keys1.concat(keys2); // all the keys
+
+  let keys = [... new Set(allKeys)]; // now we use Set to get the unique values - found this in the Mozilla documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+  let final = {}; // empty object to use later
+
+  for (let i = 0; i < keys.length; i ++) {
+    if (keys1.includes(keys[i]) && keys2.includes(keys[i])) { // if this key is in both
+      final[keys[i]] = true;
+    } else {
+      final[keys[i]] = false;
+    }
+  }
+
+  return final; //return result
+};
+
+export const questionFour = (string) => {
+  // Implement question 4 here
+
+  let firstArray = string.split("\n"); // split the string into an array by new line
+  let secondArray = []; // empty array to house final elements
+
+  firstArray.forEach((element) => { // for each element
+    let elements = element.split(","); // split element by `,`
+    secondArray.push(elements); // add this new array to the master array
+  });
+
+  return secondArray; //return result
+};
+
+
+export const studentInfo = {
+  firstName: 'Mitchell',
+  lastName: 'Reiff',
+  studentId: '10440996'
+};
---
+++ Assignments/Lab1/lab1.test.mjs
@@ -0,0 +1,60 @@
+// I pledge my honor that I have abided by the Stevens Honor System.
+
+import * as lab1 from "./lab1.mjs";
+
+//TODO: Write and call each function in lab1.js 5 times each, passing in different input
+
+// Question one
+console.log(lab1.questionOne([1, 2, 4]));                                       // { '73': true }
+console.log(lab1.questionOne([10, 15]));                                        // { '4375': false }
+console.log(lab1.questionOne([16, 201, 192, 18]));                              // { '15208417': false }
+console.log(lab1.questionOne([75, 50, 18]));                                    // { '552707': true }
+console.log(lab1.questionOne([12, 16, 18]));                                    // { '11656': false }
+
+
+// Question two
+console.log(lab1.questionTwo([1, 2, 3, 4, 2]));                                 // [ false, 3, 4 ]
+console.log(lab1.questionTwo([7, 8, 9, 10, 11]));                               // [ true ]
+console.log(lab1.questionTwo([10, 9, 11, 15]));                                 // [ false, 0, 1 ]
+console.log(lab1.questionTwo([1, 7, 7, 8]));                                    // [ false, 1, 2 ]
+console.log(lab1.questionTwo([2019, 2020, 2021, 2022, 2023]));                  // [ true ]
+
+
+// Question three
+console.log(lab1.questionThree({x:1, y:2, z:3}, {y:7, w:99, x:1, z:8}));                // { x: true, y: true, z: true, w: false }
+console.log(lab1.questionThree({hello:"world", bob:"I'm Bob"}, {hello:17}));            // { hello: true, bob: false }
+console.log(lab1.questionThree({happy:"good", w2:"tax"}, {w2:"form", happy:"yay"}));    // { happy: true, w2: true }
+console.log(lab1.questionThree({e:12, w:":)", a:[1,2]}, {a:1, e:8}));                   // { e: true, w: false, a: true }
+console.log(lab1.questionThree({https:"443", http:"80"}, {ssh:"22", ftp:"21"}));        // { https: false, http: false, ssh: false, ftp: false }
+
+
+// Question four
+console.log(                                                                    // [ 'Matthew', 'Wade', 'ide402' ],[ 'Alex', 'Wellerstein', 'hhs130' ],[ 'Brunella', 'Taddeo', 'bio281' ]
+  lab1.questionFour(`Matthew,Wade,ide402
+Alex,Wellerstein,hhs130
+Brunella,Taddeo,bio281`)
+);
+
+console.log(                                                                    // [ 'Kevin', 'Ryan', 'cpe551' ],[ 'Frank', 'Riccobono', 'ee552' ],[ 'Yousef', 'Abdelmalek', 'ee553' ]
+  lab1.questionFour(`Kevin,Ryan,cpe551
+Frank,Riccobono,ee552
+Yousef,Abdelmalek,ee553`)
+);
+
+console.log(                                                                    // [ 'Dov', 'Kruger', 'cpe390' ],[ 'Mukund', 'Iyengar', 'cpe360' ],[ 'Denis', 'Ovchinnikov', 'ma134' ]
+  lab1.questionFour(`Dov,Kruger,cpe390
+Mukund,Iyengar,cpe360
+Denis,Ovchinnikov,ma134`)
+);
+
+console.log(                                                                    // [ 'Chan', 'Yu', 'e120' ],[ 'Rami', 'Bashour', 'e121' ],[ 'Michael', 'Schoch', 'cal103' ]
+  lab1.questionFour(`Chan,Yu,e120
+Rami,Bashour,e121
+Michael,Schoch,cal103`)
+);
+
+console.log(                                                                    // [ 'Kevin', 'Lu', 'cpe423' ],[ 'Jan', 'Schaumann', 'cs615' ],[ 'Joe', 'Mindak', 'ide400' ]
+  lab1.questionFour(`Kevin,Lu,cpe423
+Jan,Schaumann,cs615
+Joe,Mindak,ide400`)
+);
