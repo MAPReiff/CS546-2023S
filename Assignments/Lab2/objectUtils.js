8d4551cacce2032669ea12b94dcb5d751198e0f4
Mitchell <me@mreiff.dev>
2023-02-04 15:20:24 -0500

    Function 6 (partly) and function 7

--- Assignments/Lab2/objectUtils.js
+++ Assignments/Lab2/objectUtils.js
@@ -3,12 +3,45 @@
       DO NOT CHANGE THE FUNCTION NAMES
 */
 
+import util from "util"; // used for the areObjectsEqual function
+
 let areObjectsEqual = (...args) => {
-      //this function takes in a variable number of objects that's what the ...args signifies
+  //this function takes in a variable number of objects that's what the ...args signifies
+
+  if (args.length < 2) {
+    throw new Error("please supply atleast two objects");
+  }
+
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] == "undefined") {
+      throw new Error("atleast one of your inputs is undefined");
+    } else if (typeof args[i] != "object") {
+      throw new Error("only objects can be passed to this function");
+    } else if (Array.isArray(args[i])) {
+      // arrays are also typeof object
+      throw new Error("only objects can be passed to this function");
+    }
+  }
+
+  // now the actual problem
+
+  let good = true; // assume we're good to start
+
+  for (let i = 0; i < args.length; i++) {
+    if (!util.isDeepStrictEqual(args[0], args[i])) {
+      // isDeepStrictEqual is a built in function that recursively evaluates equality
+      // https://nodejs.org/api/util.html#util_util_isdeepstrictequal_val1_val2
+      // https://nodejs.org/api/assert.html#assertdeepstrictequalactual-expected-message
+      good = false;
+      break;
+    }
+  }
+
+  return good;
 };
 
 let calculateObject = (object, funcs) => {};
 
 let combineObjects = (...args) => {
-      //this function takes in a variable number of objects that's what the ...args signifies
+  //this function takes in a variable number of objects that's what the ...args signifies
 };
--- Assignments/Lab2/stringUtils.js
+++ Assignments/Lab2/stringUtils.js
@@ -118,4 +118,73 @@ export let censorWords = (string, badWordsList) => {
   return final;
 };
 
-export let distance = (string, word1, word2) => {};
+export let distance = (string, word1, word2) => {
+  // this *works* in the most basic sense. Does not work for multi word and doesnt validate is word1 is before word2.
+  // will come back to this if time permits, if not pray for partial credit
+
+  if (typeof string == "undefined") {
+    throw new Error("no string supplied");
+  } else if (typeof string != "string") {
+    throw new Error("please supply a string as the first parameter");
+  } else if (string.length == 0) {
+    throw new Error("input string can not be empty");
+  } else if (string.replace(/ /g, "") == "") {
+    throw new Error("input string can not be only whitespace");
+  } else if (string.split(" ") < 2) {
+    throw new Error("input string must be atleast 2 words long");
+  }
+
+  string = string.toLowerCase();
+
+  if (typeof word1 == "undefined") {
+    throw new Error("no word1 supplied");
+  } else if (typeof word1 != "string") {
+    throw new Error("word1 must be a string");
+  } else if (word1.length == 0) {
+    throw new Error("word1 must not be empty");
+  } else if (word1.replace(/ /g, "") == "") {
+    throw new Error("word1 can not be only whitespace");
+  } else if (!string.includes(word1.toLowerCase())) {
+    throw new Error("word1 must be part of the input string");
+  }
+
+  word1 = word1.toLowerCase();
+
+  if (typeof word2 == "undefined") {
+    throw new Error("no word2 supplied");
+  } else if (typeof word2 != "string") {
+    throw new Error("word2 must be a string");
+  } else if (word2.length == 0) {
+    throw new Error("word2 must not be empty");
+  } else if (word2.replace(/ /g, "") == "") {
+    throw new Error("word2 can not be only whitespace");
+  } else if (!string.includes(word2.toLowerCase())) {
+    throw new Error("word2 must be part of the input string");
+  }
+
+  word2 = word2.toLowerCase();
+
+  if (word1 == word2) {
+    return 0; // distance between itself is 0
+  }
+
+  // check if word 1 comes before word 2
+  let input = string.split(" ");
+  let finalLength = input.length;
+
+  let count1 = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] == word1) {
+      for (let j = 0; j < input.length; j++) {
+        if (input[j] == word2) {
+          if (i - j - 1 < finalLength) {
+            finalLength = Math.abs(i - j);
+          }
+        }
+      }
+    }
+  }
+
+  return finalLength;
+};
