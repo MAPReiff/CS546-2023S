806831e87819af5ccce53d8675c197bbd3c93955
Mitchell <me@mreiff.dev>
2023-02-10 16:15:13 -0500

    Function 4

--- Assignments/Lab3/test.js
+++ Assignments/Lab3/test.js
@@ -1,8 +1,41 @@
 import { getMovies, getUsers } from "./helpers.js";
-import { getUserById } from "./users.js";
+import { getUserById, moviesReviewed } from "./users.js";
 
 const referMovies = async (id) => {
   let user = await getUserById(id);
   let userName = user.username;
-  let fav = user.avorite_genre.toLowerCase().trim();
+  let fav = user.favorite_genre.toLowerCase().trim();
+
+  if (fav == "(no genres listed)") {
+    return [];
+  }
+
+  let movies = await getMovies();
+
+  let recomend = [];
+
+  for (let i = 0; i < movies.length; i++) {
+    // let genres = movies[i].genre.split("|")
+    let movieReviews = movies[i].reviews;
+    if (
+      movies[i].genre.trim().toLowerCase().split("|").includes(fav) &&
+      movieReviews.length > 0
+    ) {
+      let good = true;
+      for (let j = 0; j < movieReviews.length; j++) {
+        if (movieReviews[j].username.trim().toLowerCase() == userName) {
+          good = false;
+          break;
+        }
+      }
+      if (good == true) {
+        recomend.push(movies[i].title)
+      }
+    }
+    // console.log(movies[i].genre.trim().toLowerCase().split("|"));
+  }
+
+  console.log(recomend);
 };
+
+await referMovies("5060fc9e-10c7-4f38-9f3d-47b7f477568b");
--- Assignments/Lab3/users.js
+++ Assignments/Lab3/users.js
@@ -87,7 +87,7 @@ export const moviesReviewed = async (id) => {
     let movieReviews = movies[i].reviews;
     if (movieReviews.length > 0) {
       for (let j = 0; j < movieReviews.length; j++) {
-        if (movieReviews[j].username.trim() == user) {
+        if (movieReviews[j].username.trim().toLowerCase() == user) {
           let ob = {};
           ob.username = user;
           ob.rating = movieReviews[j].rating;
@@ -103,4 +103,39 @@ export const moviesReviewed = async (id) => {
   return reviews;
 };
 
-const referMovies = async (id) => {};
+export const referMovies = async (id) => {
+  let user = await getUserById(id);
+  let userName = user.username;
+  let fav = user.favorite_genre.toLowerCase().trim();
+
+  if (fav == "(no genres listed)") {
+    return [];
+  }
+
+  let movies = await getMovies();
+
+  let recomend = [];
+
+  for (let i = 0; i < movies.length; i++) {
+    // let genres = movies[i].genre.split("|")
+    let movieReviews = movies[i].reviews;
+    if (
+      movies[i].genre.trim().toLowerCase().split("|").includes(fav) &&
+      movieReviews.length > 0
+    ) {
+      let good = true;
+      for (let j = 0; j < movieReviews.length; j++) {
+        if (movieReviews[j].username.trim().toLowerCase() == userName) {
+          good = false;
+          break;
+        }
+      }
+      if (good == true) {
+        recomend.push(movies[i].title);
+      }
+    }
+    // console.log(movies[i].genre.trim().toLowerCase().split("|"));
+  }
+
+  console.log(recomend);
+};
