31b19f3f782b59939325cf1d7a022cfb7b56fc90
Mitchell Reiff <me@mreiff.dev>
2023-03-08 13:58:19 -0500

    Add lab 4 code to lab 6

--- Assignments/Lab6/helpers.js
+++ Assignments/Lab6/helpers.js
@@ -1,3 +1,36 @@
 // I pledge my honor that I have abided by the Stevens Honor System.
 
 // You can add and export any helper functions you want here - if you aren't using any, then you can just leave this file as is
+
+import { ObjectId } from "mongodb";
+
+export const idToOID = (input) => {
+  if (typeof input == "undefined") {
+    throw new Error("please provide a valid ID string");
+  } else if (typeof input != "string") {
+    throw new Error("please provide a valid ID string");
+  }
+
+  input = input.trim();
+
+  if (input.length == 0) {
+    throw new Error("ObjectID can not be blank or spaces");
+  }
+
+  if (oIDChecker(input) == false) {
+    throw new Error("the provided id is not a valid ObjectID for mongo");
+  }
+
+  let final = new ObjectId(input)
+
+  return final;
+}
+
+export const oIDChecker = (input) => {
+  try {
+    ObjectId.isValid(new ObjectId(input));
+    return true;
+  } catch (e) {
+    return false
+  }
+}
