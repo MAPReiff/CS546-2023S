a0e86fb06ffbeb57c4a6a7ba080cd6be6da72fa0
Mitchell Reiff <me@mreiff.dev>
2023-03-09 20:53:26 -0500

    data index

--- Assignments/Lab6/data/index.js
+++ Assignments/Lab6/data/index.js
@@ -1,3 +1,10 @@
 // I pledge my honor that I have abided by the Stevens Honor System.
 
 // This file should import both data files and export them as shown in the lecture code
+
+// I feel this is pointless... why not just import the files directly when needed rather than having this index file?
+import * as albumFuncs from "./albums.js";
+import * as bandFuncs from "./bands.js";
+
+export const albums = albumFuncs;
+export const bands = bandFuncs;
