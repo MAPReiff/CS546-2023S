93e0cee64c1a712b77d0d68d7ab0656304b17e45
Mitchell Reiff <me@mreiff.dev>
2023-04-17 15:13:24 -0400

    Lab10 Login/Register Basic Forms

--- Assignments/Lab10/routes/auth_routes.js
+++ Assignments/Lab10/routes/auth_routes.js
@@ -1,42 +1,64 @@
 //import express, express router as shown in lecture code
-import {Router} from "express"
-export const router = Router()
+import { Router } from "express";
+export const router = Router();
 
-router.route('/').get(async (req, res) => {
+router.route("/").get(async (req, res) => {
   //code here for GET THIS ROUTE SHOULD NEVER FIRE BECAUSE OF MIDDLEWARE #1 IN SPECS.
-  return res.json({error: 'YOU SHOULD NOT BE HERE!'});
+  return res.json({ error: "YOU SHOULD NOT BE HERE!" });
 });
 
 router
-  .route('/register')
+  .route("/register")
   .get(async (req, res) => {
     //code here for GET
+    try {
+      res.status(200).render("register", { title: "Register" });
+    } catch (e) {
+      res
+        .status(500)
+        .render("error", {
+          title: "Error",
+          error: "internal server error",
+          code: "500",
+        });
+    }
   })
   .post(async (req, res) => {
     //code here for POST
   });
 
 router
-  .route('/login')
+  .route("/login")
   .get(async (req, res) => {
     //code here for GET
+    try {
+      res.status(200).render("login", { title: "Login" });
+    } catch (e) {
+      res
+        .status(500)
+        .render("error", {
+          title: "Error",
+          error: "internal server error",
+          code: "500",
+        });
+    }
   })
   .post(async (req, res) => {
     //code here for POST
   });
 
-router.route('/protected').get(async (req, res) => {
+router.route("/protected").get(async (req, res) => {
   //code here for GET
 });
 
-router.route('/admin').get(async (req, res) => {
+router.route("/admin").get(async (req, res) => {
   //code here for GET
 });
 
-router.route('/error').get(async (req, res) => {
+router.route("/error").get(async (req, res) => {
   //code here for GET
 });
 
-router.route('/logout').get(async (req, res) => {
+router.route("/logout").get(async (req, res) => {
   //code here for GET
 });
