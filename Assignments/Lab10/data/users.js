92f6be3dc516acfa106dec721c1ed02cb386317d
Mitchell Reiff <me@mreiff.dev>
2023-04-17 14:34:19 -0400

    Lab10 data/users

--- Assignments/Lab10/data/users.js
+++ Assignments/Lab10/data/users.js
@@ -1,10 +1,98 @@
-//import mongo collections, bcrypt and implement the following data functions
+//import mongo collections, bcrypt and implement the following emailAddress functions
+
+import { users } from "../config/mongoCollections.js";
+import { checkName, checkEmail, checkPassword } from "../helpers.js";
+import { ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
+
 export const createUser = async (
   firstName,
   lastName,
   emailAddress,
   password,
   role
-) => {};
+) => {
+  firstName = checkName(firstName, "first name");
+  lastName = checkName(lastName, "last name");
+  emailAddress = checkEmail(emailAddress);
 
-export const checkUser = async (emailAddress, password) => {};
+  const userCollection = await users();
+  const existingEmailUser = await userCollection.findOne({
+    emailAddress: {
+      $eq: emailAddress,
+    },
+  });
+
+  if (existingEmailUser != null) {
+    throw new Error("a user with this email already exists");
+  }
+
+  password = checkPassword(password);
+
+  if (typeof role == "undefined") {
+    throw new Error("please supply a role string");
+  } else if (typeof role != "string") {
+    throw new Error("please supply a role string");
+  }
+
+  role = role.trim().toLowerCase();
+
+  if (role != "user" && role != "admin") {
+    throw new Error("role string must be either user or admin");
+  }
+
+  let user = {
+    firstName: firstName,
+    lastName: lastName,
+    emailAddress: emailAddress,
+    password: await bcrypt.hashSync(password, 15),
+    role: role,
+  };
+
+  let newUser = await userCollection.insertOne(user);
+
+  if (!newUser.acknowledged || !newUser.insertedId) {
+    throw new Error("unable to add the user");
+  }
+
+  return { insertedUser: true };
+};
+
+export const checkUser = async (emailAddress, password) => {
+  emailAddress = checkEmail(emailAddress);
+  password = checkPassword(password);
+
+  const userCollection = await users();
+  const existingEmailUser = await userCollection.findOne({
+    emailAddress: {
+      $eq: emailAddress,
+    },
+  });
+
+  if (existingEmailUser == null) {
+    throw new Error("Either the email address or password is invalid");
+  }
+
+  if (!(await bcrypt.compareSync(password, existingEmailUser.password))) {
+    throw new Error("Either the email address or password is invalid");
+  }
+
+  return {
+    firstName: existingEmailUser.firstName,
+    lastName: existingEmailUser.lastName,
+    emailAddress: existingEmailUser.emailAddress,
+    role: existingEmailUser.role,
+  };
+};
+
+// console.log(
+//   await createUser(
+//     "Mitchell",
+//     "Reiff",
+//     "mreiff@stevEns.edu",
+//     "HorsePull748*%",
+//     "user"
+//   )
+// );
+
+// console.log(await checkUser("mreiff@stevens.edu", "HorsePull748*%"));
--- Assignments/Lab10/helpers.js
+++ Assignments/Lab10/helpers.js
@@ -1,1 +1,85 @@
 //You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
+import * as EmailValidator from "email-validator";
+import passwordValidator from "password-validator";
+
+export const checkName = (data, type) => {
+  if (typeof data == "undefined") {
+    throw new Error(`please provide a ${type} string`);
+  } else if (typeof data != "string") {
+    throw new Error(`please provide a ${type} string`);
+  }
+
+  data = data.trim();
+
+  if (data.replaceAll(" ", "").length == 0) {
+    throw new Error(`${type} string must contain text and not only spaces`);
+  }
+
+  if (data.length < 2) {
+    throw new Error(`${type} must be atleast 2 characters long`);
+  } else if (data.length > 25) {
+    throw new Error(`${type} must be no longer than 25 characters`);
+  }
+
+  if (/\d/.test(data)) {
+    throw new Error(`${type} can not contain numbers`);
+  }
+
+  return data;
+};
+
+export const checkEmail = (emailAddress) => {
+  if (typeof emailAddress == "undefined") {
+    throw new Error("please provide an email string");
+  } else if (typeof emailAddress != "string") {
+    throw new Error("please provide an email string");
+  }
+
+  emailAddress = emailAddress.trim().toLowerCase();
+
+  if (emailAddress.replaceAll(" ", "").length == 0) {
+    throw new Error("email string must contain text and not only spaces");
+  }
+
+  if (!EmailValidator.validate(emailAddress)) {
+    throw new Error("please enter a valid email address");
+  }
+
+  return emailAddress;
+};
+
+export const checkPassword = (password) => {
+  if (typeof password == "undefined") {
+    throw new Error("please supply a password string");
+  } else if (typeof password != "string") {
+    throw new Error("please supply a password string");
+  }
+
+  if (password.replaceAll(" ", "").length == 0) {
+    throw new Error(
+      "password string must contain a password and not only spaces"
+    );
+  }
+
+  if (password.length < 8) {
+    throw new Error("passwords must be atleast 8 characters long");
+  }
+
+  let passwordParams = new passwordValidator()
+    .is()
+    .min(8)
+    .has()
+    .uppercase()
+    .has()
+    .digits()
+    .has()
+    .symbols();
+
+  if (!passwordParams.validate(password)) {
+    throw new Error(
+      "your passwords must conatin at least one uppercase character, there has to be at least one number and there has to be at least one special character"
+    );
+  }
+
+  return password;
+};
